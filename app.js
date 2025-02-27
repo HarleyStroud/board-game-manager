fa28ee81d58ca78a86ee4564ca5865a97e64f3ea
HarleyStroud <stroudhd@gmail.com>
2025-02-27 15:47:08 -0500

    Ensure styles.css is correctly loaded from public directory

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const path = require('path');
+const bodyParser= require('body-parser');
 const app = express();
 const port = 3000;
 const { getHomePage} = require('./routes/index');
+const game = require('./routes/game');
 
-// Set EJS as the view engine
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files from "public"
 app.use(express.static('public'));
 
 // Routes
 app.get('/', getHomePage);
+app.get('/add-game', game.getAdd);
+app.post('/add-game', game.postAdd);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
